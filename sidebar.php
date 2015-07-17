80b4a4a8d3fe51b62c4609ce1d0471bde9b09c47
Marco Antonio Konopacki <marcoamarelo@gmail.com>
2015-07-17 11:45:48 -0300

    criação da sidebar e inclusao no front-page e single

---
+++ sidebar.php
@@ -0,0 +1,56 @@
+<div class="col-md-4">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h4 class="red font-roboto">Sobre o programa de intercâmbio:</h4>
+        </div>
+        <div class="panel-body">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p>Sed elementum vulputate nunc sed lobortis. </p>
+            <p>Curabitur ultrices fringilla nulla</p>
+        </div>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <div class="edicoes">
+                <h4 class="red font-roboto">Mais edições</h4>
+                <ul class="list-group">
+                    <li class="list-group-item"><a href="#">Cras justo odio</a></li>
+                    <li class="list-group-item"><a href="#">Dapibus ac facilisis in</a></li>
+                    <li class="list-group-item"><a href="#">Morbi leo risus</a></li>
+                    <li class="list-group-item"><a href="#">Porta ac consectetur ac</a></li>
+                    <li class="list-group-item"><a href="#">Vestibulum at eros</a></li>
+                </ul>
+            </div>
+            <div class="editais divider-top">
+                <h4 class="red font-roboto">Editais</h4>
+                <ul class="list-group">
+                    <li class="list-group-item"><a href="#">Cras justo odio</a></li>
+                    <li class="list-group-item"><a href="#">Dapibus ac facilisis in</a></li>
+                    <li class="list-group-item"><a href="#">Morbi leo risus</a></li>
+                    <li class="list-group-item"><a href="#">Porta ac consectetur ac</a></li>
+                    <li class="list-group-item"><a href="#">Vestibulum at eros</a></li>
+                </ul>
+            </div>
+            <div class="categorias divider-top">
+                <h4 class="red font-roboto">Categorias</h4>
+                <ul class="list-unstyled">
+                    <li><a href="#">Cras justo odio</a></li>
+                    <li><a href="#">Dapibus ac facilisis in</a></li>
+                    <li><a href="#">Morbi leo risus</a></li>
+                    <li><a href="#">Porta ac consectetur ac</a></li>
+                    <li><a href="#">Vestibulum at eros</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h4 class="red font-roboto">Promovido por:</h4>
+        </div>
+        <div class="panel-body">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p>Sed elementum vulputate nunc sed lobortis. </p>
+            <p>Curabitur ultrices fringilla nulla</p>
+        </div>
+    </div>
+</div>
---
+++ single.php
@@ -0,0 +1,18 @@
+<?php
+get_header(); ?>
+    <div class="container">
+        <div class="row">
+            <?php get_template_part('menu', 'horizontal'); ?>
+        </div>
+        <?php
+        // Start the Loop.
+        while (have_posts()) : the_post();
+            // Include the page content template.
+            get_template_part('content', 'single');
+            comments_template();
+        endwhile;
+        ?>
+        <?php get_sidebar(); ?>
+    </div>
+<?php
+get_footer();
