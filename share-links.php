2beb07b25e3c4b15b6b8b30232723998c89b0d25
Marco Antonio Konopacki <marcoamarelo@gmail.com>
2015-07-22 10:52:41 -0300

    incluída as funções pra apresentar os links de compartilhamento do post

--- share-links.php
+++ share-links.php
@@ -1,5 +1,11 @@
 <div class="row divider-top">
     <div class="col-xs-12 text-right">
-        <a href="#" class="btn btn-default btn-sm"><i class="fa fa-share-alt"></i> compartilhe</a>
+        <a href="#" onclick="jQuery('#share-box-<?php the_ID(); ?>').slideToggle('fast'); return false;" class="btn btn-default btn-sm"><i class="fa fa-share-alt"></i> compartilhe</a>
+
+        <?php if (function_exists('ssb_share_icons')): ?>
+        <div id="share-box-<?php the_ID(); ?>" style="display: none;">
+            <?php echo ssb_share_icons(); ?>
+        </div>
+        <?php endif; ?>
     </div>
 </div>
